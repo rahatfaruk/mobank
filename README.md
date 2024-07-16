df97d144ab47c096c4029e39c9867f6a3cac2068
rahatfaruk <rahatfaruk99@gmail.com>
2024-07-16 12:57:02 +0600

    init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mobank
+MFS type mobile banking system with mern stack. User can send money, cash-out, and see balance inquiries etc. Developed by Rahat Faruk. Project idea has given by programming hero. 
+
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (  
+    <div>
+      <h1 className="p-3 flex items-center gap-1.5 text-2xl text-center font-semibold">
+        <span className="px-1 rounded bg-indigo-600 text-xl text-white">MB</span>
+        <span className="text-indigo-600">Mobank</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
