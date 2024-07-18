64ccfdf554fecd27c08d0f0fc5ab2582031bf321
rahatfaruk <rahatfaruk99@gmail.com>
2024-07-18 11:19:42 +0600

    add: ErrorPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router-dom'
-import App from './App.jsx'
 import './index.css'
+import App from './App.jsx'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
 import Home from './pages/Home.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App/>} errorElement={'error'} >
-      {/* <Route index element={ <Navigate to={'/login'} replace /> } /> */}
+    <Route path='/' element={<App/>} errorElement={ <ErrorPage/> } >
       <Route index element={ <Home/> } />
       <Route path='login' element={ <Login/> } />
       <Route path='register' element={ <Register/> } />
